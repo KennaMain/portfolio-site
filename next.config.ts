a004f34e0ae70d2e5256d98fb74a6ac67361ce1b
Clay <claytonharting@gmail.com>
2025-07-25 15:03:50 -0400

    removed static generation for heroku deployment

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   assetPrefix: isProd ? '/portfolio-site/' : '',
-  output: 'export', // For static export
-  images: {
-    unoptimized: true, // For static export
-  },
 };
 
 export default nextConfig;
