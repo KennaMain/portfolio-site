38dedc84905f20309a710e1426fcba2e9d46dc94
Clay <claytonharting@gmail.com>
2025-07-25 15:41:35 -0400

    trying out another way to deploy site

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  assetPrefix: isProd ? '/portfolio-site/' : '',
+  // assetPrefix: isProd ? '/portfolio-site/' : '',
+  output: 'export', // For static export
+  // images: {
+  //   unoptimized: true, // For static export
+  // },
+  // Ensure JavaScript isn't being disabled
+  reactStrictMode: true,
 };
 
 export default nextConfig;
