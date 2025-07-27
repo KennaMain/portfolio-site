0f0f3b0e02165d17d3bca069d83dfd8e80537e12
Clay <claytonharting@gmail.com>
2025-07-27 11:17:53 -0400

    added tracking eyes to logo squid

--- src/components/MouseTrackingSeahorse.tsx
+++ src/components/MouseTrackingSeahorse.tsx
@@ -1,39 +1,12 @@
 'use client'
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Box } from '@mui/material';
+import { useMouseAngleTracking } from '../hooks/UseMouseAngleTracking';
 
 const MouseTrackingSeahorse: React.FC = () => {
-  const [angle, setAngle] = useState(0);
-  const prevAngleRef = useRef(0);
-
-  // Handle mouse movement to rotate the image
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      // Get center position of the rotating image
-      const centerX = window.innerWidth - 170; // 100px from right
-      const centerY = window.innerHeight - 210; // 200px from bottom (stacked)
-      
-      // Calculate angle between center and cursor
-      const dx = e.clientX - centerX;
-      const dy = e.clientY - centerY;
-      let newAngle = Math.atan2(dy, dx) * 180 / Math.PI + 180;
-      
-      // Smooth angle transitions when crossing the 360° boundary
-      const prevAngle = prevAngleRef.current;
-      const diff = newAngle - prevAngle;
-      
-      // If the difference is more than 180°, we've crossed the boundary
-      if (diff > 180) newAngle -= 360;
-      if (diff < -180) newAngle += 360;
-      
-      prevAngleRef.current = newAngle;
-      setAngle(newAngle);
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => window.removeEventListener('mousemove', handleMouseMove);
-  }, []);
+  const eyeElementRef = useRef<HTMLImageElement>(null) || undefined
+  const { angle } = useMouseAngleTracking({ref: eyeElementRef})
 
   return (
     <>
@@ -71,6 +44,7 @@ const MouseTrackingSeahorse: React.FC = () => {
         }}
       >
         <img 
+          ref={eyeElementRef}
           src="site-assets/seahorse_eye.svg"
           alt="Seahorse Eye"
           style={{ width: '100%', height: '100%', objectFit: 'contain' }}
