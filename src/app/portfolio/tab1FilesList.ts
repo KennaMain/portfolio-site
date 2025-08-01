c7387c3ec7c206e60a2c8de34d548627e2c8c1ff
Clay <claytonharting@gmail.com>
2025-08-01 10:14:55 -0400

    added project-based organization to tab1 files list

--- src/app/portfolio/tab1FilesList.ts
+++ src/app/portfolio/tab1FilesList.ts
@@ -58,4 +58,155 @@ export const imagePaths: string[] = [
   "/portfolio/tab1/psa/psa_2.jpg",
   "/portfolio/tab1/psa/psa_1.jpg",
 ]
+
+export type Project = {
+  title: string,
+  description: string,
+  thumbnail: string | undefined,
+  files: string[]
+}
+
+export const projects: Project[] = [
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/patterns/main_w1 2.jpg",
+      "/portfolio/tab1/patterns/pattern6.jpg",
+      "/portfolio/tab1/patterns/horseshoecrab.jpg",
+      "/portfolio/tab1/patterns/diatoms.png",
+      "/portfolio/tab1/patterns/kennamain_pattern_1.jpg",
+      "/portfolio/tab1/patterns/radiolarian.png",
+      "/portfolio/tab1/patterns/cuttlefish.png",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/metamorphic/main_metamorphic.png",
+      "/portfolio/tab1/metamorphic/TRANSFORMER poster.png",
+      "/portfolio/tab1/metamorphic/shapeshifter poster.png",
+      "/portfolio/tab1/metamorphic/metamorphic poster final.png",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/Gauache/main_poster_art.jpg",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/bookstore/bookposter2.png",
+      "/portfolio/tab1/bookstore/bookposter3.png",
+      "/portfolio/tab1/bookstore/bookposter1.png",
+      "/portfolio/tab1/bookstore/Three_Posters_In_Metal_Frames.jpg",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/make_a_discovery/make4.png",
+      "/portfolio/tab1/make_a_discovery/make5.png",
+      "/portfolio/tab1/make_a_discovery/make7.png",
+      "/portfolio/tab1/make_a_discovery/make6.png",
+      "/portfolio/tab1/make_a_discovery/make2.png",
+      "/portfolio/tab1/make_a_discovery/makefront.png",
+      "/portfolio/tab1/make_a_discovery/make3.png",
+      "/portfolio/tab1/make_a_discovery/IMG_7681.jpg",
+      "/portfolio/tab1/make_a_discovery/IMG_7678.jpg",
+      "/portfolio/tab1/make_a_discovery/make.glb",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/park_wayfinding/softball4.png",
+      "/portfolio/tab1/park_wayfinding/softball3.png",
+      "/portfolio/tab1/park_wayfinding/softball2.png",
+      "/portfolio/tab1/park_wayfinding/softball1.png",
+      "/portfolio/tab1/park_wayfinding/park_signs.png",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/pompeiiworm_inforgraphic/pompeii infographic.pdf",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/lbtp/lbtp_direwolf_sticker.png",
+      "/portfolio/tab1/lbtp/KennaMain_TshirtMockupfront_ 2.jpg",
+      "/portfolio/tab1/lbtp/lbtp_poster.png",
+      "/portfolio/tab1/lbtp/KennaMain_PinMockup.jpg",
+      "/portfolio/tab1/lbtp/KennaMain_TicketMockup.jpg",
+      "/portfolio/tab1/lbtp/lbtp_wordlockup.svg",
+      "/portfolio/tab1/lbtp/KennaMain_TshirtMockupback_2.jpg",
+      "/portfolio/tab1/lbtp/KennaMain_ToteBag_Mockup_2.jpg",
+      "/portfolio/tab1/lbtp/lbtp_symbol.svg",
+      "/portfolio/tab1/lbtp/KennaMain_Sticker_Mockup_2.jpg",
+      "/portfolio/tab1/lbtp/KennaMain_ToteBag_Mockup_1.jpg",
+      "/portfolio/tab1/lbtp/KennaMain_cardmockup_2.jpg",
+      "/portfolio/tab1/lbtp/KennaMain_cardmockup_1.jpg",
+      "/portfolio/tab1/lbtp/KennaMain_StickerMockup.1.jpg",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/book/book_illustrations.png",
+      "/portfolio/tab1/book/bookcover.glb",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/science_illustrations/cicada.png",
+      "/portfolio/tab1/science_illustrations/marine_worm.png",
+      "/portfolio/tab1/science_illustrations/lynx_spider.png",
+      "/portfolio/tab1/science_illustrations/florida_insects.png",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/newsletter/makingwherever.png",
+      "/portfolio/tab1/newsletter/whoiam.png",
+      "/portfolio/tab1/newsletter/front.png",
+    ]
+  },
+  {
+    title: "",
+    description: "",
+    thumbnail: undefined,
+    files: [
+      "/portfolio/tab1/psa/psa_3.jpg",
+      "/portfolio/tab1/psa/psa_2.jpg",
+      "/portfolio/tab1/psa/psa_1.jpg",
+    ]
+  },
 ]
