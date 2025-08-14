cdcd080ba76c5cbcd8b096d1c40c7af07a8e7f87
Kenna Main <kennamain@Kennas-MacBook-Pro.local>
2025-08-14 09:22:23 -0400

    updated text on the about me page

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -5,12 +5,10 @@ import { GridLegacy as Grid, Typography } from '@mui/material';
 import { DarkBackgroundLeftOffsetBox } from '../../components/DarkBackgroundLeftOffsetBox';
 import { theme } from '@/theme';
 import "../globals.css";
-import GLTFViewer from '@/components/GLTFViewer';
 
 export default function AboutMe() {
   return (
     <DarkBackgroundLeftOffsetBox>
-      <GLTFViewer/>
       <Grid container spacing={0}>
         {/* Left Column - Image */}
         <Grid item xs={12} md={4}>
@@ -25,10 +23,14 @@ export default function AboutMe() {
 
           <Typography variant="body1" paragraph sx={{ mb: 3, fontSize: '1.1rem' }}>
           I have a background in both graphic design and biology, and I find that the two influence each other quite a bit. I received my bachelors of science at the University of South Florida In 2023. It was there that I majored in marine biology and minored in studio art. I found so much fulfillment in my studio art courses, that I then decided to pursue a certificate of graphic design, which I received in 2025 from the Rhode Island school of design. 
+          </Typography>
 
-While taking online courses at RISD, I became proficient in layout design, typography, creating vector designs in Adobe Illustrator, editing and enhancing work in Photoshop, arranging images and text in InDesign, and the design research workflow necessary for design projects. I make careful considerations about composition, how to bring visual interest into designs, the readability of text, how information flows, and how the intended audience will interpret and respond to different designs.
+          <Typography variant="body1" paragraph sx={{ mb: 3, fontSize: '1.1rem' }}>    
+Much of my design inspiration comes from observations I make in my environment and biological concepts. I am inspired by the morphology and biological functions observed in nature, so when I can incorporate these into design work, I do. It is often a design goal of mine to reference or educate about such interesting, beautiful, and strange scientific concepts through my work. 
+          </Typography>
 
-As a graphic designer I look to incorporate observation, research, visual interest, flow, surprise and storytelling. My goal is to inspire conversation, inform, and share with others through design.
+          <Typography variant="body1" paragraph sx={{ mb: 3, fontSize: '1.1rem' }}>
+As a graphic designer I look to incorporate observation, research, visual interest, flow, surprise, and storytelling. My goal is to inspire conversation, inform, and share with others through design.
           </Typography>
 
         </Grid>
