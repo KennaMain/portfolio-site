d552aba89580b41555a5a2b763256114faa63ba0
Clay <claytonharting@gmail.com>
2025-08-13 20:24:56 -0400

    fixed lighting

--- src/components/GLTFViewer.tsx
+++ src/components/GLTFViewer.tsx
@@ -70,8 +70,8 @@ function Scene({ color, controlsEnabled }: { color?: string, controlsEnabled: bo
       {color && <color attach="background" args={[color]} />}
       <ambientLight intensity={0.5} />
       <pointLight position={[20, 30, 10]} intensity={1} />
-      <pointLight position={[-10, -10, -10]} color="blue" />
-      <Environment preset="dawn" />
+      <pointLight position={[-10, -10, -10]} />
+      <Environment preset='studio'/>
       <PerspectiveCamera 
         ref={cameraRef}
         fov={40} 
